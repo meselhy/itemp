e664ba941b369719e5257f4c6c48d5611b098f60
Abdalla Meselhy <76181509+meselhy@users.noreply.github.com>
2021-05-19 13:54:36 +0300

    Update main.py

--- pi/main.py
+++ pi/main.py
@@ -6,6 +6,10 @@ import board
 import adafruit_amg88xx
 import requests
 from statistics import mean
+from pyzbar import pyzbar
+from pydub import AudioSegment
+from pydub.playback import play
+
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
@@ -13,20 +17,6 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 # checking the username from  qr reader is mandatory for the temp to run
 # the code should be waiting the user to inter qr code infront of cam
 
-def main():
-    u = readUsr()
-    #tempRead()
-    g = isGuest(u)
-    if g == True
-        t = tempRead()
-        upDb(u, t)
-        main()
-    else
-        t = tempRad()
-        toDb(u, t)
-        main()
-
-main()
 
 #Scan usernames from QR reader RETURN username
 def readUsr():
@@ -37,50 +27,48 @@ def readUsr():
         ret, frame = camera.read()
         barcodes = pyzbar.decode(frame)
         for barcode in barcodes:
-           x, y , w, h = barcode.rect
-           barcode_text = barcode.data.decode('utf-8')
-           #print(barcode_text)
-           return barcode_text
-           cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+            x, y , w, h = barcode.rect
+            barcode_text = barcode.data.decode('utf-8')
+            #print(barcode_text)
+            return barcode_text
+            cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
         cv2.imshow('Barcode reader', frame)
-        if cv2.waitKey(1) and barcode_text!="": #0xFF == 27:
+        if cv2.waitKey(1) and 0xFF == 27: #barcode_text!="": 
             break
 # free camera object and exit
-cap.release()
-cv2.destroyAllWindows()
+    camera.release()
+    cv2.destroyAllWindows()
 
 
 # Read temprature and return it
 def tempRead():
     if True:
-    	for row in amg.pixels:
-        	#time.sleep(0)
-        	xtemp = ['{0:0.2f}'.format(temp) for temp in row]
-	rslt = list(map(float, xtemp))
-	avg = mean(rslt)
-	#print("Average of the list =", round(avg, 2))
-    if avg == 0:
-        return tempRead()
-    elif avg != 0:
-        if avg >= 35 or avg <= 40:
-            #send it to db
+        for row in amg.pixels:
+            #time.sleep(0)
+            xtemp = ['{0:0.2f}'.format(temp) for temp in row]
+        rslt = list(map(float, xtemp))
+        avg = 0
+        avg = mean(rslt)
+        #print("Average of the list =", round(avg, 2))
+        if avg == 0:
+            tempRead()
+        elif avg != 0:
+            if avg >= 35 or avg <= 40:
+                #send it to db
+                return avg
+            else:
+                tempRead()
+        else:
             return avg
-        else
-            return tempRead()
-            #OR
-            exit()
-    else
-        return avg
-        exit()
 
 # Check if the user is guest or not RETUNR TRUE/FALSE
 def isGuest(x):
     #call readUsr() to check if the user is guest or not / if true todb will be used else updb
-    x = readUsr()
+    #x = readUsr()
     userdata = {"username": x}
     resp = requests.get('https://itemp.ml/app/isguest.php', params=userdata, headers={"User-agent":"ab"})
     #print(resp.text)
-    if resp.text == 1:
+    if resp.text == "1":
         return True
     else:
         return False
@@ -92,7 +80,7 @@ def toDb(usr, tempp):
     userdata = {"username": usr, "temp": tempp}
     resp = requests.get('https://itemp.ml/app/todb.php', params=userdata, headers={"User-agent":"ab"})
     #print(resp.status_code)
-    #print(resp.text)
+    return(resp.text)
 
 # Update updated temp to database, Paramaters are username & temp  #for regestred users
 def upDb(usr, tempp):
@@ -100,4 +88,34 @@ def upDb(usr, tempp):
     userdata = {"username": usr, "temp": tempp}
     resp = requests.get('https://itemp.ml/app/updb.php', params=userdata, headers={"User-agent":"ab"})
     #print(resp.status_code)
-    #print(resp.text)
+    return(resp.text)
+    
+
+def main():
+    u = readUsr()
+    #tempRead()
+    g = isGuest(u)
+    if g == True:
+        t = tempRead()
+        if t >= 36 or t <= 38:
+            song = AudioSegment.from_wav("ok.wav")
+            play(song)
+            upDb(u, t)
+            main()
+        else:
+            song = AudioSegment.from_wav("err.wav")
+            play(song)
+            main()
+    if g == False:
+        t = tempRead()
+        if t >= 36 or t <= 38:
+            song = AudioSegment.from_wav("ok.wav")
+            play(song)
+            toDb(u, t)
+            main()
+        else:
+            song = AudioSegment.from_wav("err.wav")
+            play(song)
+            main()
+            
+main()
