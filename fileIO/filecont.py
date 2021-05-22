fff291fac993ff5edbb7703e77122c4f1cf8ae6d
FREDYJ300 <64916174+FREDYJ300@users.noreply.github.com>
2021-05-22 02:26:40 -0400

    Add files via upload

--- fileIO/filecont.py
+++ fileIO/filecont.py
@@ -5,3 +5,20 @@ f.write(username,phone,temp)
 else:
 f=open("userfile.txt","W")
 f.write(fullname,phone,temp)
+
+
+if guest == True:
+f=open("guestfile","r")
+for line in f
+cont=line.split(',')
+username=cont[0]
+phone=cont[1]
+temp=cont[2]
+
+else:
+f=open("userfile","r")
+for line in f
+cont=line.split(',')
+fullname=cont[0]
+phone=cont[1]
+temp=cont[2]
